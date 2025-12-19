1185d804236ee3b53705e1bfe2df861964f88203
JUNIORDECHAVES <juniorchaves0010@gmail.com>
2025-12-19 12:04:03 -0300

    retirando bug silencioso

--- src/Server.ts
+++ src/Server.ts
@@ -3,10 +3,9 @@ import { env } from "./config/env";
 
 const server = new App();
 
-if (process.env.NODE_ENV !== "production"){
     server.app.listen(env.PORT, () => {
         console.log(`🚀 API rodando na porta ${env.PORT}`);
     });
-}
+
 
 
