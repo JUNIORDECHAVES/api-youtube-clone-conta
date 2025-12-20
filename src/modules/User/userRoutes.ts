db9309e1a52857f049ec58b32588239c7355b93f
JUNIORDECHAVES <juniorchaves0010@gmail.com>
2025-12-20 09:26:48 -0300

    modificação do nome da rota de login

--- src/modules/User/userRoutes.ts
+++ src/modules/User/userRoutes.ts
@@ -13,7 +13,7 @@ class UserRoutes {
 
     private initializeRoutes() {
         this.router.post("/sign-up", this.controller.createUser);
-        this.router.get("/login", this.controller.login);
+        this.router.get("/get-user", this.controller.login);
         this.router.get("/list", this.controller.list);
 
     }
